eb9da30470f5103824972e028d48fd4aa734e097
chirag <sharmachirag2899@gmail.com>
2025-02-10 19:13:50 +0530

    Adding Transition to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { HiArrowTurnDownRight } from "react-icons/hi2";
 
 const Button = ({ title = "Get Started" }) => {
     return (
-        <div className="px-3 sm:px-4 py-2 rounded-2xl bg-zinc-200 text-black flex items-center justify-between gap-2 sm:gap-3">
+        <div className="px-3 sm:px-4 py-2 rounded-2xl bg-zinc-200 text-black flex items-center justify-between gap-2 sm:gap-3 transition-transform duration-500 hover:scale-110">
             <span className="text-xs sm:text-sm font-bold">{title}</span>
             <HiArrowTurnDownRight className="w-4 h-4 sm:w-5 sm:h-5" />
         </div>
